824bbe0b258acb0ced66bd6814a1a5437ba487bd
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-10-08 01:40:58 +0000

    trying to get Wiki API call to work

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  url = "http://en.wikipedia.org/w/api.php?action=parse&page=Tom%20Cruise&format=json"
+  http_response = HTTP.get(url) #what is this returning?
+  parsed_response = JSON.parse(http_response) #getting JSON::ParserError at / unexpected token at ''
+  parse = parsed_response.fetch("parse")
+  @text = parse.fetch("text")
+  
+  erb(:test)
 end
